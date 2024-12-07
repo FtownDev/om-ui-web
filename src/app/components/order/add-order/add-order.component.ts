5c79d2020338cf05a38f51bd1583d5b68f721702
b3cecchini <b3cecchini@gmail.com>
2024-12-07 01:54:36 -0500

    Del/PU logic and starting adding items logic

--- src/app/components/order/add-order/add-order.component.ts
+++ src/app/components/order/add-order/add-order.component.ts
@@ -8,6 +8,9 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OrderItem } from '@/src/app/models/Order/OrderItem';
+import { InventoryService } from '@/src/app/services/inventory.service';
+import { InventoryCategory } from '@/src/app/models/Inventory/InventoryCategory';
+import { InventoryItem } from '@/src/app/models/Inventory/InventoryItem';
 
 @Component({
   selector: 'app-add-order',
@@ -22,21 +25,33 @@ export class AddOrderComponent implements OnInit {
   showDeliveryModal: Boolean = false;
   tempDeliveryTimes: string[][] = [];
   showPickupModal: Boolean = false;
+  specifyPickupTimes: Boolean = false;
   tempPickupTimes: string[][] = [];
   private destroy$ = new Subject<void>();
   orderContext: Order | null = null;
   orderService = inject(OrderService);
   customerService = inject(CustomerService);
+  iventroryService = inject(InventoryService);
   customerContext: Customer | null = null;
   eventTypes: EventType[] = [];
   addressContext: Address[] | null = [];
   fb = inject(FormBuilder);
 
+  // Technical debt item, accidently made OrderItem need an OrderId on the create request, but order Id doesnt exist until after creation
+  // Will not affect anything because on the backend this value gets overwritten with the actual ID
+  orderId_temp = crypto.randomUUID();
+  //
+
   orderForm: FormGroup<any> | undefined;
   itemsForm: FormGroup<any> | undefined;
   deliveryWindowForm: FormGroup | undefined;
+  pickupWindowForm: FormGroup | undefined;
+  orderItemForm: FormGroup | undefined;
   orderItems: OrderItem[] = [];
 
+  itemCategories: InventoryCategory[] = [];
+  items: InventoryItem[] = [];
+  filteredItems: InventoryItem[] = [];
   selectedShippingAddress: Address | null = null;
 
   ngOnInit(): void {
@@ -60,6 +75,15 @@ export class AddOrderComponent implements OnInit {
       startTime: ['', Validators.required],
       endTime: ['', Validators.required],
     });
+    this.pickupWindowForm = this.fb.group({
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+    });
+
+    this.orderItemForm = this.fb.group({
+      itemId: ['', Validators.required],
+      qty: [''],
+    });
 
     this.customerService.customerContext$
       .pipe(takeUntil(this.destroy$))
@@ -82,14 +106,74 @@ export class AddOrderComponent implements OnInit {
       this.eventTypes = res;
       this.orderService.setEventsContext(res);
     });
+
+    this.iventroryService.getInventoryCategories().subscribe((res) => {
+      this.itemCategories = res;
+    });
+
+    this.iventroryService.getInventoryItems().subscribe((res) => {
+      this.items = res;
+    });
   }
 
   get f() {
     return this.orderForm!.controls;
   }
+  get i() {
+    return this.orderItemForm!.controls;
+  }
 
   get d() {
     return this.deliveryWindowForm!.controls;
+  }
+  get p() {
+    return this.pickupWindowForm!.controls;
+  }
+
+  getItemName(itemId: string) {
+    return this.items.find((f) => f.id == itemId)?.name;
+  }
+
+  getItemCategoryName(itemId: string) {
+    return this.itemCategories.find(
+      (c) => c.id == this.items.find((f) => f.id == itemId)?.categoryId
+    )?.name;
+  }
+
+  getItemPrice(itemId: string) {
+    return this.items.find((f) => f.id == itemId)?.price;
+  }
+
+  onItemCategoryChange(event: any) {
+    const category = this.itemCategories?.find((c) => c.id == event.value);
+    if (category) {
+      this.filteredItems = this.items.filter(
+        (i) => i.categoryId == category.id
+      );
+    }
+  }
+
+  onSelectItemChange(event: any) {
+    const item = this.filteredItems.find((i) => i.id == event.value);
+    if (item) {
+      this.orderItemForm!.get('itemId')!.setValue(item.id);
+    }
+  }
+
+  addOrderItem() {
+    if (this.orderItemForm!.valid) {
+      const formVal = this.orderItemForm!.value;
+      console.log(formVal);
+      const newItem: OrderItem = {
+        itemId: formVal.itemId,
+        qty: formVal.qty,
+        orderId: this.orderId_temp,
+      };
+
+      console.log('newItem', newItem);
+      this.orderItems.push(newItem);
+      this.orderItemForm?.reset();
+    }
   }
 
   addDeliveryWindow() {
@@ -98,7 +182,28 @@ export class AddOrderComponent implements OnInit {
       const newVal = [formVal.startTime, formVal.endTime];
       this.tempDeliveryTimes.push(newVal);
       this.showDeliveryModal = false;
+      this.deliveryWindowForm.reset();
     }
+  }
+
+  resetDeliverWindows() {
+    this.specifyDeliverTimes = false;
+    this.tempDeliveryTimes = [];
+  }
+
+  addPickupWindow() {
+    if (this.pickupWindowForm?.valid) {
+      const formVal = this.pickupWindowForm!.value;
+      const newVal = [formVal.startTime, formVal.endTime];
+      this.tempPickupTimes.push(newVal);
+      this.showPickupModal = false;
+      this.pickupWindowForm.reset();
+    }
+  }
+
+  resetPickupWindows() {
+    this.specifyPickupTimes = false;
+    this.tempPickupTimes = [];
   }
 
   onShippingAddressChange(event: any) {
