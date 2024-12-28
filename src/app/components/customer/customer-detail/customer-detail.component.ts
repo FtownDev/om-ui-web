cdbb078d6e8bf6faf5b9726ad1f16bd02f1e31e4
b3cecchini <b3cecchini@gmail.com>
2024-12-27 21:04:12 -0500

    Setting up Customer Detail page

--- src/app/components/customer/customer-detail/customer-detail.component.ts
+++ src/app/components/customer/customer-detail/customer-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CustomerService } from '@/src/app/services/customer.service';
 import { Customer } from '@/src/app/models/Customer/Customer';
+import { Address } from '@/src/app/models/Address/Address';
 
 @Component({
   selector: 'app-customer-detail',
@@ -9,12 +10,33 @@ import { Customer } from '@/src/app/models/Customer/Customer';
   styleUrl: './customer-detail.component.css',
 })
 export class CustomerDetailComponent implements OnInit {
+  // Flags
+  isLoading = false;
+  error = false;
+  // Context
   customerContext: Customer | null = null;
+  addressContext: Address[] | null = null;
+
+  // Services
   customerService = inject(CustomerService);
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.customerService.customerContext$.subscribe((value) => {
       this.customerContext = value;
+      if (value !== null) {
+        this.loadAddresses(value.id);
+      }
+      this.isLoading = false;
+    });
+    this.customerService.addressContext$.subscribe((value) => {
+      this.addressContext = value;
+    });
+  }
+
+  loadAddresses(customerId: string): void {
+    this.customerService.getAddresses(customerId).subscribe((value) => {
+      this.customerService.setaddressContext(value);
     });
   }
 }
